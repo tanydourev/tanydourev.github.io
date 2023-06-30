d7a27bdd93dca435377aafcec022ddafaf734b2f
Tany Dourev <74632648+tanydourev@users.noreply.github.com>
2023-06-30 15:36:04 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,10 @@
 // body fade-in on load
-
 document.body.classList.add('fade-out');
 window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('fade-out');
 });
 
-
 // navigation triggers
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
 }
@@ -15,3 +12,49 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// gallery carousel
+$(document).ready() {
+    $('.gallery-carousel').flickity({
+        wrapAround: true,
+        prevNextButtons: true,
+        pageDots: true,
+        accessibility: true
+    });
+});
+
+// rw posts carousel
+$(document).ready() {
+    $('.rw-posts-carousel').flickity({
+        wrapAround: true,
+        prevNextButtons: true,
+        pageDots: false
+    });
+});
+
+// animals carousel
+$(document).ready() {
+    $('.animals-carousel').flickity({
+        wrapAround: true,
+        prevNextButtons: true,
+        pageDots: true
+    });
+});
+
+// environments carousel
+$(document).ready() {
+    $('.environments-carousel').flickity({
+        wrapAround: true,
+        prevNextButtons: true,
+        pageDots: true
+    });
+});
+
+// eco domes carousel
+$(document).ready() {
+    $('.eco-domes-carousel').flickity({
+        wrapAround: true,
+        prevNextButtons: true,
+        pageDots: true
+    });
+});
