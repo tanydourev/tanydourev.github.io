ab1a34c4fd25466dc72de3ec6036d29aeffbb5e5
Tany Dourev <74632648+tanydourev@users.noreply.github.com>
2023-07-05 17:41:37 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,32 +12,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
-// flickity carousels
-var flkty = new Flickity('.gallery-carousel', {
-    wrapAround: true,
-    prevNextButtons: false,
-    pageDots: true,
-    accessibility: true
-});
-
-var flkty = new Flickity('.animals-carousel', {
-    wrapAround: true,
-    prevNextButtons: true,
-    pageDots: true,
-    accessibility: false
-});
-
-var flkty = new Flickity('.environments-carousel', {
-    wrapAround: true,
-    prevNextButtons: true,
-    pageDots: true,
-    accessibility: false
-});
-
-var flkty = new Flickity('.eco-domes-carousel', {
-    wrapAround: true,
-    prevNextButtons: true,
-    pageDots: true,
-    accessibility: false
-});
