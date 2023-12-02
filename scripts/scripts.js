d4e36c1cea8ecb1fab676776362786df24b0f071
Tany Dourev <74632648+tanydourev@users.noreply.github.com>
2023-12-02 02:43:54 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,29 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// about section buttons
+function showAbout() {
+    document.getElementById("showAbout").style.display = "block";
+    document.getElementById("showConnect").style.display = "none";
+}
+
+function showConnect() {
+    document.getElementById("showAbout").style.display = "none";
+    document.getElementById("showConnect").style.display = "block";
+}
+
+// fade elements
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
