9922590e0071f1c9752c139f582f056ba63cf1a7
Tany Dourev <74632648+tanydourev@users.noreply.github.com>
2024-10-24 14:58:32 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,19 +14,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("responsive-navigation").style.width = "0";
 }
-
-// preloader
-const loader = document.getElementById("preloader");
-
-const fadeOutEffect = setInterval(() => {
-    if (!preloader.style.opacity) {
-      preloader.style.opacity = 1;
-    }
-    if (preloader.style.opacity > 0) {
-      preloader.style.opacity -= 0.1;
-    } else {
-      clearInterval(fadeEffect);
-    }
-  }, 100);
-
-  window.addEventListener('load', fadeOutEffect);
